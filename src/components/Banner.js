519b019a6dd933acdfd4716fcf6073e5c454988d
mgcreations <info@mgcreations.co.za>
2021-10-10 12:30:41 +0200

    Setting up components for project

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+const Banner = (props) => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>MG Creations</h1>
+      </header>
+      <div className="content">
+        <p>
+          Website and Graphic Design
+          <br />
+          with stunning results.
+        </p>
+      </div>
+    </div>
+  </section>
+);
+
+export default Banner;
