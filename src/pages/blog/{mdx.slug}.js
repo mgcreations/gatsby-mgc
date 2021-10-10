a1fcefbe866848efd979d93ef78147445f376923
mgcreations <info@mgcreations.co.za>
2021-10-10 19:35:37 +0200

    Styled Post page

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
@@ -9,11 +10,32 @@ const BlogPost = ({ data }) => {
   const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>Author: {data.mdx.frontmatter.author}</p>
-      <p>{data.mdx.frontmatter.date}</p>
-      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
-      <p>Photo Credit: {data.mdx.frontmatter.hero_image_credit_text}</p>
-      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      <Helmet>
+        <title>{data.mdx.frontmatter.title}</title>
+      </Helmet>
+      <div id="main" className="alt">
+        <section id="one">
+          <div className="inner">
+            <header className="major">
+              <h1>{data.mdx.frontmatter.title}</h1>
+            </header>
+            <p>
+              Author: {data.mdx.frontmatter.author}
+              {"  "}|{"  "}
+              {data.mdx.frontmatter.date}
+            </p>
+            <span className="image main">
+              <GatsbyImage
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+              />
+              <p>Photo Credit: {data.mdx.frontmatter.hero_image_credit_text}</p>
+            </span>
+
+            <MDXRenderer>{data.mdx.body}</MDXRenderer>
+          </div>
+        </section>
+      </div>
     </Layout>
   );
 };
